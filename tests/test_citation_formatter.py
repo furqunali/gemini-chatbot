69c504bf5f73983c7f3bc7f681f4194eb34894d1
Furqan Ali <furqanali2628@gmail.com>
2026-09-21 01:03:24 +0500

    feat: add test_citation_formatter

---
+++ tests/test_citation_formatter.py
@@ -0,0 +1,7 @@
+from citation_formatter import append_citations, format_citations
+
+def test_format_citations_is_deterministic():
+    assert format_citations(["b","a","a"])=="[a] [b]"
+
+def test_append_citations_handles_empty_values():
+    assert append_citations("answer",["","c1"])=="answer [c1]"
